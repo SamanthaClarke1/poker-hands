ff41d7f689b48398b99bd3d467bfc01cc5e16a80
Samuel-Clarke123 <clarkepearl44@gmail.com>
2021-04-10 18:48:53 +1000

    moved streamsplit compilation method

--- build/streamsplit.js
+++ build/streamsplit.js
@@ -11,89 +11,74 @@ WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
 ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
 IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 */
-
-'use strict'
-
-var through = require('syncthrough')
-var StringDecoder = require('string_decoder').StringDecoder
-
-function transform (chunk) {
-  this._last += this._decoder.write(chunk)
-  if (this._last.length > this.maxLength) {
-    this.emit('error', new Error('maximum buffer reached'))
-    return
-  }
-
-  var list = this._last.split(this.matcher)
-
-  this._last = list.pop()
-
-  for (var i = 0; i < list.length; i++) {
-    push(this, this.mapper(list[i]))
-  }
+'use strict';
+var through = require('syncthrough');
+var StringDecoder = require('string_decoder').StringDecoder;
+function transform(chunk) {
+    this._last += this._decoder.write(chunk);
+    if (this._last.length > this.maxLength) {
+        this.emit('error', new Error('maximum buffer reached'));
+        return;
+    }
+    var list = this._last.split(this.matcher);
+    this._last = list.pop();
+    for (var i = 0; i < list.length; i++) {
+        push(this, this.mapper(list[i]));
+    }
 }
-
-function flush () {
-  // forward any gibberish left in there
-  this._last += this._decoder.end()
-
-  if (this._last) {
-    push(this, this.mapper(this._last))
-  }
+function flush() {
+    // forward any gibberish left in there
+    this._last += this._decoder.end();
+    if (this._last) {
+        push(this, this.mapper(this._last));
+    }
 }
-
-function push (self, val) {
-  if (val !== undefined) {
-    self.push(val)
-  }
+function push(self, val) {
+    if (val !== undefined) {
+        self.push(val);
+    }
 }
-
-function noop (incoming) {
-  return incoming
+function noop(incoming) {
+    return incoming;
 }
-
-function split (matcher, mapper, options) {
-  // Set defaults for any arguments not supplied.
-  matcher = matcher || /\r?\n/
-  mapper = mapper || noop
-  options = options || {}
-
-  // Test arguments explicitly.
-  switch (arguments.length) {
-    case 1:
-      // If mapper is only argument.
-      if (typeof matcher === 'function') {
-        mapper = matcher
-        matcher = /\r?\n/
-      // If options is only argument.
-      } else if (typeof matcher === 'object' && !(matcher instanceof RegExp)) {
-        options = matcher
-        matcher = /\r?\n/
-      }
-      break
-
-    case 2:
-      // If mapper and options are arguments.
-      if (typeof matcher === 'function') {
-        options = mapper
-        mapper = matcher
-        matcher = /\r?\n/
-      // If matcher and options are arguments.
-      } else if (typeof mapper === 'object') {
-        options = mapper
-        mapper = noop
-      }
-  }
-
-  var stream = through(transform, flush)
-
-  stream._last = ''
-  stream._decoder = new StringDecoder('utf8')
-  stream.matcher = matcher
-  stream.mapper = mapper
-  stream.maxLength = options.maxLength
-
-  return stream
+function split(matcher, mapper, options) {
+    // Set defaults for any arguments not supplied.
+    matcher = matcher || /\r?\n/;
+    mapper = mapper || noop;
+    options = options || {};
+    // Test arguments explicitly.
+    switch (arguments.length) {
+        case 1:
+            // If mapper is only argument.
+            if (typeof matcher === 'function') {
+                mapper = matcher;
+                matcher = /\r?\n/;
+                // If options is only argument.
+            }
+            else if (typeof matcher === 'object' && !(matcher instanceof RegExp)) {
+                options = matcher;
+                matcher = /\r?\n/;
+            }
+            break;
+        case 2:
+            // If mapper and options are arguments.
+            if (typeof matcher === 'function') {
+                options = mapper;
+                mapper = matcher;
+                matcher = /\r?\n/;
+                // If matcher and options are arguments.
+            }
+            else if (typeof mapper === 'object') {
+                options = mapper;
+                mapper = noop;
+            }
+    }
+    var stream = through(transform, flush);
+    stream._last = '';
+    stream._decoder = new StringDecoder('utf8');
+    stream.matcher = matcher;
+    stream.mapper = mapper;
+    stream.maxLength = options.maxLength;
+    return stream;
 }
-
-module.exports = split
+module.exports = split;
